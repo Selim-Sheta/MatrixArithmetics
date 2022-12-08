291d08773f0383781987d676bc24a5e8baf07d29
SelimSheta <selim.sheta@l-acoustics.com>
2022-12-08 14:58:05 +0000

    fixed vector distance

--- MatrixArithmetics.h
+++ MatrixArithmetics.h
@@ -497,7 +497,7 @@ namespace arrmath {
         }
         T sum = static_cast<T>(0.0);
         for (size_t i = 0; i < length; i++) {
-            sum += pow(vec1[0] - vec2[0], 2);
+            sum += pow(vec1[i] - vec2[i], 2);
         }
         return sqrt(sum);
     }
